eaea0c940761cc704a160cce978a41985e048753
SpaceKookie <sabel.katharina@gmail.com>
2015-02-21 16:58:09 +0100

    Adding src again...woops

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+# =========================================================
+# Copyright: (c) 2015 Katharina Sabel
+# License  : LGPL 3.0 (See LICENSE)
+# Comment  : Init file required for building. 
+#			 Contains Version number.
+# =========================================================
+
+VERSION = "0.1"
