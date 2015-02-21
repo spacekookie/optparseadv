f211bde788a243acd4bd7c69f929a97f8980f64f
SpaceKookie <sabel.katharina@gmail.com>
2015-02-21 16:26:55 +0100

    Changing test file

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-# =========================================================
-# Copyright: (c) 2015 Katharina Sabel
-# License  : LGPL 3.0 (See LICENSE)
-# Comment  : Init file required for building. 
-#			 Contains Version number.
-# =========================================================
-
-VERSION = "0.1"
--- tests/parser_test.py
+++ tests/parser_test.py
@@ -7,25 +7,18 @@
 
 # Prepare application path
 import sys
-sys.path.append('../src/')
+sys.path.append('../lib/')
 
 import adv_opt_parse as parser
 
-def connect(master, sub, data):
-	pass
-	# print "This is a connect to", sub, "with data", data
+def connect(master, slave, sub, data):
+	print master, slave, sub, data
 
-def copy(master, sub, data):
-	pass
-	# print "This is a copy with", sub, "and", data
-
-p = parser.OptParseAdv({'connect':connect, 'copy':copy})
-# p.enable_debug()
-p.add_suboptions('connect', {'-X': (None, parser.__VALUE__)})
-p.add_suboptions('copy', {'--file': (None, parser.__FIELD__)})
-
-p.sub_aliases('connect', {'-X': ['-X']})
-p.sub_aliases('copy', {'--file': ['-f', '--file']})
+p = parser.OptParseAdv({'connect':connect})
+p.set_master_field('connect', True)
 p.master_aliases('connect', ['c'])
+p.define_slaves({'nas':'192.168.2.131'})
+p.add_suboptions('connect', {'-X': (None, parser.__VALUE__), '--command': (None, parser.__FIELD__)})
+p.sub_aliases('connect', {'-X': ['-X'], '--command': ['-c']})
 
-p.parse('copy --file=/path/to/file')
+p.parse('c nas -X')
