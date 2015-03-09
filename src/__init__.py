b963aa51f315a9aa551a664986da8ae71bcf440f
Katharina Sabel <sabel.katharina@gmail.com>
2015-03-09 18:03:41 +0100

    Fixing src setup

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+# =========================================================
+# Copyright: (c) 2015 Katharina Sabel
+# License  : GPL 2.0 (See LICENSE)
+# Comment  : Init file with version number
+#
+# =========================================================
+
+import pkg_resources
+__VERSION__ = pkg_resources.require("advoptparse")[0].version
